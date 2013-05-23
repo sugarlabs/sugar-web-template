d0f373f23a88e3936a566a94f3c66058e26bcb7c
Manuel Quiñones <manuq@laptop.org>
2013-05-23 11:24:33 -0300

    Adapt to icon rename

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,6 @@
 define(function (require) {
     var activity = require("sugar-html-activity/activity");
-    var icons = require("sugar-html-graphics/icons");
+    var icon = require("sugar-html-graphics/icon");
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
@@ -11,7 +11,7 @@ define(function (require) {
         // Colorize the activity icon.
         var activityButton = document.getElementById("activity-button");
         activity.getXOColor(function (colors) {
-            icons.colorize(activityButton, colors);
+            icon.colorize(activityButton, colors);
         });
 
         // Make the activity stop with the stop button.
